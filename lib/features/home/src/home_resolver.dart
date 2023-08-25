0ba1324eee9471377126629a42d6103478e64ec0
MohammadDwery <mohammaddwery7ce@gmail.com>
2023-08-25 18:36:41 +0300

    FIXES:     - Move home_injection_module.dart

--- lib/features/home/src/home_resolver.dart
+++ lib/features/home/src/home_resolver.dart
@@ -1,7 +1,7 @@
-import 'package:pokemon/features/splash/src/di/root_screen_injection_module.dart';
 import '../../../infrastructure/core/modules/feature_resolver.dart';
 import '../../../infrastructure/core/modules/injection_module.dart';
 import '../../../infrastructure/core/modules/router_module.dart';
+import 'di/home_injection_module.dart';
 import 'presentation/router/home_router_module.dart';
 
 class HomeResolver extends FeatureResolver {
---
+++ lib/features/home/src/presentation/screens/home_screen_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreenBloc {
+  final textEditingController = TextEditingController();
+
+}
